a93be18b33f3de2e47c77d37e0dfef4035e318c2
Minda Shiraishi <mademoiselle@Mindas-Laptop.local>
2026-05-06 11:57:57 +0800

    bind server to 0.0.0.0 for Railway

--- server.js
+++ server.js
@@ -85,6 +85,6 @@ app.post('/flights/book', async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log('Server running on http://localhost:' + PORT);
 });
