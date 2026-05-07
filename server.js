fc926db235845cbd892000252d175131bce871f1
Minda Shiraishi <mademoiselle@Mindas-Laptop.local>
2026-05-07 18:08:54 +0800

    remove duplicate route and fix backtick

--- server.js
+++ server.js
@@ -83,10 +83,8 @@ app.post('/flights/book', async (req, res) => {
   }
 
 
-});app.get("/", (req, res) => {
-  res.send("Flight API is running");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
